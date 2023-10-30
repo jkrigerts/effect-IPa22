e2a9351c01c8779ead310b7fcaedde405652552c
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-10-30 15:20:37 +0200

    Spread operator added for ovject

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
-      <ToDo todo={todo} />
+      <ToDo {...todo} />
     </div>
   );
 }
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 function ToDo(props) {
-  const [check, setCheck] = useState(props.todo.completed);
+  const [check, setCheck] = useState(props.completed);
 
   function handleCheck(event) {
     setCheck(event.target.checked);
   }
   return (
     <>
-      <p>{props.todo.title}</p>
-      <p>userID: {props.todo.userId}</p>
-      <p>toDoId: {props.todo.id}</p>.
+      <p>{props.title}</p>
+      <p>userID: {props.userId}</p>
+      <p>toDoId: {props.id}</p>.
       <input type="checkbox" checked={check} onChange={handleCheck} />
     </>
   );
